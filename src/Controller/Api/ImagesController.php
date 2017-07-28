1c742889c860b792ffae25be4088d127c20e854b
Peter de Beer <pdebeer@transip.nl>
2017-07-29 01:15:32 +0200

    Add image index call

--- src/Controller/Api/ImagesController.php
+++ src/Controller/Api/ImagesController.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Controller\Api;
 
 use App\Controller\AppController;
+use App\Model\Entity\Image;
+use App\Model\Entity\Tag;
 
 /**
  * Images Controller
@@ -13,6 +16,8 @@ use App\Controller\AppController;
 class ImagesController extends AppController
 {
 
+
+
     /**
      * Index method
      *
@@ -20,9 +25,26 @@ class ImagesController extends AppController
      */
     public function index()
     {
-        $images = $this->Images->find()->limit(10);
 
-        $this->set('data',  $images->toArray());
+
+        $tags = $this->Images->Tags->find()->contain(['Images']);
+        $data = [];
+        /** @var Tag $tag */
+        foreach ($tags as $tag) {
+            if (!isset($tag->images[0]->id)) {
+                continue;
+            }
+            $data[] = [
+                'tag' => $tag->name,
+                'previewHeight' => $tag->images[0]->imageWidth,
+                'previewWidth' => $tag->images[0]->imageWidth,
+                'imageWidth' => $tag->images[0]->imageWidth,
+                'imageHeight' => $tag->images[0]->imageWidth,
+                'image' => base64_encode(file_get_contents($tag->images[0]->previewURL)),
+                'imaageId' => $tag->images[0]->id
+            ];
+        }
+        $this->set('data', $data);
         $this->set('success', true);
         $this->set('message', null);
         $this->set('_serialize', ['data', 'success', 'message']);
@@ -32,6 +54,7 @@ class ImagesController extends AppController
      * View method
      *
      * @param string|null $id Image id.
+     *
      * @return \Cake\Http\Response|void
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
@@ -71,6 +94,7 @@ class ImagesController extends AppController
      * Edit method
      *
      * @param string|null $id Image id.
+     *
      * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
@@ -97,6 +121,7 @@ class ImagesController extends AppController
      * Delete method
      *
      * @param string|null $id Image id.
+     *
      * @return \Cake\Http\Response|null Redirects to index.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
